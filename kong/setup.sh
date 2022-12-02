e1f25cded1ee4847f4b3ed4972e689047b32cc1b
chronolaw <chrono_cpp@me.com>
2022-12-02 21:11:00 +0800

    kong kic cmd examples

--- kong/setup.sh
+++ kong/setup.sh
@@ -15,3 +15,14 @@ wget -O- 127.1:8100/status
 # nodeport svc
 curl 192.168.10.210:30777 -I
 curl 192.168.10.210:30777 |jq
+
+# replace 30612 to real nodeport
+curl $(minikube ip):30777 -H 'host: kong.test' -i
+curl $(minikube ip):30777 -H 'host: kong.dev' -i
+curl $(minikube ip):30777 -H 'host: kong.ops' -i
+
+# port-forward
+kubectl port-forward -n kong ingress-kong-dep-dc785bd49-2f7dw 8000 &
+
+curl --resolve kong.dev:8000:127.0.0.1 kong.dev:8000 -v
+curl --resolve kong.ops:8000:127.0.0.1 kong.ops:8000 -v
